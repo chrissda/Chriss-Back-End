3c7c90ad0f1843cacce9fff44392603042371a67
chrissda <jhersonbarreto88@hotmail.com>
2024-07-11 21:27:14 -0500

    variables

---
+++ dia 1/1_variables.py
@@ -0,0 +1,40 @@
+nombre = 'Eduardo'
+
+edad = 12
+
+soltero = True # Boolean > solamente o es True o es False
+
+sueldo = 870.35
+
+# Tipos de Datos Primivitos
+
+nombre = -1
+
+# En python los tipos de variables puedem cabiar sin ninguna restriccion
+
+# java | c# | C++ no se puede cambiar el tipo de dato de una vez creado
+
+# Cada tipo de dato de las variables tiene sus propiedades y métodos definidos
+
+# Las variables siempre deben empezar con letras
+
+texto1 = 'Hola yo me llamo '
+texto2 = 'Cristhian'
+
+resultado = texto1 + texto2
+print(resultado)
+
+alumno1, alumno2, alumno3, = 'Ignacio', 'Segundo', 'Abel'
+
+print(alumno1)
+
+# upper > convierte el contenido a MAYUSCULAS
+# Para todo la informacion relacionada a las variables TEXTO:
+# 
+print(alumno1.upper())
+
+# 
+
+# split > separar el texto en base al caracter de busqueda
+resultado = texto1.split(" ")
+print(resultado)
